8df678f09d90c6593e3a5fff8335d6931e0cf5aa
Nikola Korać <koracn423@gmail.com>
2026-04-13 14:25:21 +0200

    Delete JAVA_WEB_APPLICATION directory

--- JAVA_WEB_APPLICATION/Java_Web_App/src/main/java/com/MyWebApp/Java_Web_App/JPArepo.java
+++
@@ -1,8 +0,0 @@
-package com.MyWebApp.Java_Web_App;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JPArepo extends JpaRepository<Products, Integer>{
-
-    
-}
--- JAVA_WEB_APPLICATION/Java_Web_App/src/main/java/com/MyWebApp/Java_Web_App/JpaUsers.java
+++
@@ -1,7 +0,0 @@
-package com.MyWebApp.Java_Web_App;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface  JpaUsers extends JpaRepository<Users, Integer>{
-    
-}
