2501bce01a2b4fd7d1dfaf780a86e4e32bcc1f85
NIkola773 <koracn423@gmail.com>
2026-05-17 14:13:29 +0200

    typescript and relations

--- JAVA_WEB_APPLICATION/Java_Web_App/src/main/java/com/MyWebApp/Java_Web_App/Products.java
+++ JAVA_WEB_APPLICATION/Java_Web_App/src/main/java/com/MyWebApp/Java_Web_App/Products.java
@@ -1,10 +1,13 @@
 package com.MyWebApp.Java_Web_App;
 
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 
 @Entity
 public class Products {
@@ -16,6 +19,9 @@ public class Products {
     private double price;
      private String username;
      
+@ManyToMany(mappedBy = "products")
+private List<Users> users;
+
     public Integer getId() {
         return id;
     }
